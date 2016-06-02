08baa3d0ec6e647edf73872c501539eddcc94485
Ferenc Fekete <Ferenc_Fekete@epam.com>
2016-06-02 16:04:24 +0200

    masodik, nem lesz jo szerintem.

--- src/main/java/org/sample/TestServlet.java
+++ src/main/java/org/sample/TestServlet.java
@@ -47,6 +47,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+ez most nem lesz jó.
+
 /**
  * @author Arun Gupta
  */
